20b7d01bf5f1055973b871b9a8ebfa3b56ec2cbf
jameswhang <sungyoonwhang2017@u.northwestern.edu>
2016-03-31 21:33:37 -0500

    adding stuff

---
+++ google_keyword.js
@@ -0,0 +1,28 @@
+$.ready(function(){ 
+    var 
+
+});
+
+var parseDOM = function(DOM) {
+
+    DOM.children().forEach(function(element) {
+        if (element.tagName === DIV) { // TODO: probably need <b>, <p> and all other shits
+
+            // TODO
+        }
+
+    });
+}
+
+var getContentLength = function(DOMString, wordLength) {
+    if (DOMString !== '') {
+        return wordLength;
+    } else {
+
+
+
+
+
+        getContentLength(DOMString, );
+    }
+}
